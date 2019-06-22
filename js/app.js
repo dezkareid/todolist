820219cf9fd7b77c1b8c17bd2a12282ceeb6c5f6
DezkaReid <elmaildeldezkareid@gmail.com>
2019-06-21 20:21:34 -0500

    added method to load tasks

---
+++ js/app.js
@@ -0,0 +1,35 @@
+/* global fetch */
+const BASE_URL = 'http://localhost:3001/'
+
+function loadList () {
+  getList()
+    .then(createList)
+    .catch(console.error)
+}
+
+function getList () {
+  return fetch(`${BASE_URL}items`)
+    .then(response => response.json())
+}
+
+function createList (listItems) {
+  const elementId = 'todo-list'
+  const listElement = document.getElementById(elementId)
+  for (const item of listItems) {
+    const itemElement = createListItem(item)
+    listElement.appendChild(itemElement)
+  }
+}
+
+function createListItem (item) {
+  const liElement = document.createElement('li')
+  liElement.innerHTML = `
+    <p>${item.text}</p>
+    <button>Eliminar</button>
+  `
+  return liElement
+}
+
+window.addEventListener('load', function () {
+  loadList()
+})
